2502d0d53148519f06e44f585c84ef3d57558695
Ewrhey <92299233+Ewrhey@users.noreply.github.com>
2021-12-02 12:06:53 +0300

    Update main.cpp

--- main.cpp
+++ main.cpp
@@ -7,17 +7,8 @@ int main()
     getline(cin, num2);
     getline(cin, znac);
     test = num1 + num2 + znac;
-    if (only_numbers(test) == false){
-            cout << "_____"<<endl;
-        if (znac == "+"){
-            cout << sum_prep(num1, num2);
-        }
-        else if (znac == "-"){
-            cout << 0;
-        }
-        else if (znac == "*"){
-            cout << 0;
-        }
+    while (only_numbers(test) == true){
+        
     }
     return 0;
 }
