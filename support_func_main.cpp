6b45b869d547ad1d89e1aa59e18ed11d4a26f735
Ewrhey <92299233+Ewrhey@users.noreply.github.com>
2021-12-02 11:19:18 +0300

    Update support_func_main.cpp

--- support_func_main.cpp
+++ support_func_main.cpp
@@ -24,3 +24,21 @@ if (znac == "*" && num1[0] != '-' && num2[0] == '-')
     return '-' + multiplication(num1, slice_str(num2, 1, len(num2)));
 if (znac == "*" && num1[0] == '-' && num2[0] != '-')
 }
+
+string chose(string num1, string num2, string znac){//znak_str1 = num1[0], znak_str2 = num2[0], znak = znac
+    string itog = "";
+    if ((num1[0] == '-' && num2[0] == '-' && znac == "-") || (num1[0] == '-' && num2[0] == '+' && znac == "+"))
+        itog = vichitator(str2, str1);
+    else if ((num1[0] == '-' && num2[0] == '+' && znak == "-") || (num1[0] == '-' && num2[0] == '-' && znac == "+"))
+        itog = "-" + (sum(num1, num2));
+    else if ((num1[0] == '+' && num2[0] == '-' && znak == "-") || (num1[0] == '+' && num2[0] == '+' && znac == "+"))
+        itog = sum(num1, num2);
+    else if ((num1[0] == '+' && num2[0] == '-' && znak == "+") || (num1[0] == '+' && num2[0] == '+' && znac == "-"))
+        itog = vichitator(str1, str2);
+    else if ((num1[0] == '-' && num2[0] == '-' && znak == "*") || (num1[0] == '+' && num2[0] == '+' && znac == "*"))
+        itog = multiplication(num1, num2);
+    else if ((num1[0] == '+' && num2[0] == '-' && znak == "*") || (num1[0] == '-' && num2[0] == '+' && znac == "*"))
+        itog = "-" + umnozhator(str1, str2);
+return itog;
+    
+}
