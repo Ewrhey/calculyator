d592531b3dc5e2d2bd343b26a70e02a6842892f1
Ewrhey <92299233+Ewrhey@users.noreply.github.com>
2021-12-02 11:46:22 +0300

    Update support_func_main.cpp

--- support_func_main.cpp
+++ support_func_main.cpp
@@ -1,5 +1,20 @@
 #include "calculyator.h"
 
+string max_num1(string num1, atring num2){
+    if(len(num1) > len(num2)){
+        return num1;
+    if(char_number(num1[0]) > char_number(num2[0]))
+        return num1;
+    return num2;
+
+string max_num1(string num1, atring num2){
+    if(len(num1) > len(num2)){
+        return num2;
+    if(char_number(num1[0]) > char_number(num2[0]))
+        return num2;
+    return num1; 
+
+
 string slice_str(string str, int start, int end_){
 if(end_ < start){
     return str;
